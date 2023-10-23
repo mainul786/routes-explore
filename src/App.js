4f6e285a25e83c87c6a693969f03602e26cf98c3
mainul96 <110577655+mainul96@users.noreply.github.com>
2023-10-23 11:03:01 +0530

    defarence type route explore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Products from './components/Products/Products';
+import About from './components/About/About';
+import Header from './components/Header/Header';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path ='home'
+         loader = {async () =>{return fetch('https://restcountries.com/v3.1/all')
+        }
+      }
+          element={<Home></Home>} ></Route>
+        <Route path='products' element={<Products></Products>}></Route>
+        <Route path='about' element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h4>About pages</h4>
+        </div>
+    );
+};
+
+export default About;
